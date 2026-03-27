3ce39a340f9af692071b9bdd51de2d090a28c43b
skshabby <shahbazshaikh418@gmail.com>
2026-03-27 09:44:41 +0530

    Removed UserModel And used Verifymodel for response

--- lib/Domain/entities/verify_otp_entity.dart
+++ lib/Domain/entities/verify_otp_entity.dart
@@ -1,9 +1,9 @@
-import 'package:jiveda_appointment/Data/model/response/user_model.dart';
+import 'package:jiveda_appointment/Data/model/response/verify_otp_response_model.dart';
 
 class VerifyOtpEntity {
   final int? success;
   final String? message;
-  final UserModel? data;
+  final VerifyData? data;
 
   VerifyOtpEntity({
     this.success,
